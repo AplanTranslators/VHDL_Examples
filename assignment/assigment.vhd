ecc70a00df78f12c8e1dd1ed805945a21f2d7f51
vladyslav-dubina <vladyslav.dubina@litsoft.com.ua>
2025-06-26 20:29:29 +0300

    update

--- assignment/assigment.vhd
+++ assignment/assigment.vhd
@@ -20,3 +20,18 @@ begin
         wait;
     end process;
 end behav;
+
+
+architecture struct of assignment_example is
+    signal a : std_logic := '0';
+begin
+    process
+        variable b : integer := 0;
+    begin
+        a <= '1';  -- Signal assignment
+        b := 5;    -- Variable assignment
+        Z <= A and B;
+        wait;
+    end process;
+end struct;
+
